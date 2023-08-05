46147423e469ad92c5e26e86bd9930e88cdc0941
OrIoN7531 <pdvorcak@gmail.com>
2023-08-06 01:47:37 +0200

    --Uo

--- client/client.lua
+++ client/client.lua
@@ -157,7 +157,14 @@ lib.callback.register('ps-housing:cb:showcase', function()
 end)
 
 function HouseTrack(propertyId)
-    local coords = PropertiesTable[propertyId].propertyData.door_data
+    local coords
+
+    if PropertiesTable[propertyId].propertyData.apartment ~= nil then
+        local getConfigName = PropertiesTable[propertyId].propertyData.apartment
+        coords = Config.Apartments[getConfigName].door
+    else
+        coords = PropertiesTable[propertyId].propertyData.door_data
+    end
 
     SetNewWaypoint(coords.x, coords.y)
 end
